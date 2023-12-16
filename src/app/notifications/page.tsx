c0016767bab297aa4b250185b6392bbbbe7f4530
Mabonga Samuel <samuelmabonga256@gmail.com>
2023-12-16 09:16:02 +0300

    commit

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -3,6 +3,14 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
 
 export default function Home() {
     return (
@@ -11,13 +19,18 @@ export default function Home() {
             <div className="md:border-x-[1px] border-gray-200 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 min-h-screen">
 
                 <div className="flex justify-between p-4 pt-8" >
-                    <h6 className="font-medium text-xl md:text-2xl">Notifications</h6>
+                    <h6 className="font-medium text-xl md:text-2xl my-auto">Notifications</h6>
+                    <Select defaultValue='personal'>
+                        <SelectTrigger className="w-[180px] text-sm md:text-base border-0 outline-none flex gap-0">
+                            <SelectValue className='text-sm md:text-base'  />
 
-                    <div className="flex gap-2 my-auto">
-                        <p className="text-sm md:text-base">Personal</p>
-                        <Badge variant="outline" className="w-fit h-fit bg-[#CC0087] text-white">20</Badge>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6" /></svg>
-                    </div>
+                            <Badge variant="outline" className="w-fit h-fit bg-[#CC0087] text-white font-medium">20</Badge>
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="personal">Personal</SelectItem>
+                            <SelectItem value="business">Business</SelectItem>
+                        </SelectContent>
+                    </Select>
                 </div>
 
                 <div className="p-4 md:p-8 flex flex-col bg-[#F4F4F4]">
@@ -29,7 +42,7 @@ export default function Home() {
 
                         <div className="flex flex-col my-auto">
                             <h4 className="md:text-lg font-medium">Mie’s beauty parlour <span className="font-thin">accepted your booking req...</span></h4>
-                            <p className="text-gray-500">$500 <span className="h-full w-[1px] bg-black" /> <span>For June 20th, 2023</span></p>
+                            <p className="text-gray-500 text-sm md:text-base">$500 <span className="h-full w-[1px] bg-black" /> <span>For June 20th, 2023</span></p>
 
                         </div>
 
@@ -44,7 +57,7 @@ export default function Home() {
 
                     <div className="flex flex-col">
                         <h4 className="md:text-lg font-medium">Deposit sent to Jane’s beauty Parlour for medium short braids.</h4>
-                        <p className="text-gray-500" >$50 <span className="h-full w-[1px] bg-black" /> <span>For June 20th, 2023</span></p>
+                        <p className="text-gray-500 text-sm md:text-base" >$50 <span className="h-full w-[1px] bg-black" /> <span>For June 20th, 2023</span></p>
 
                     </div>
                     <p className="text-gray-500 text-right text-sm">10m</p>
@@ -61,7 +74,7 @@ export default function Home() {
 
                         <div className="flex flex-col my-auto">
                             <h4 className="md:text-lg font-medium">Rebecca Wright  sent you a message.</h4>
-                            <p className="text-gray-500">Hey are you available to do these conrows for me.....</p>
+                            <p className="text-gray-500 text-sm md:text-base">Hey are you available to do these conrows for me.....</p>
                         </div>
 
                     </div>
@@ -75,9 +88,9 @@ export default function Home() {
 
                     <div className="flex flex-col gap-1">
                         <h4 className="md:text-lg font-medium">You have cancelled your Natural glam look appointment.</h4>
-                        <p className="flex gap-2">
+                        <p className="flex gap-2 text-sm md:text-base">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-credit-card text-[#CC0087]"><rect width="20" height="14" x="2" y="5" rx="2" /><line x1="2" x2="22" y1="10" y2="10" /></svg>
-                            <span className="font-bol">$70 charged</span>
+                            <span className="font-medium my-auto">$70 charged</span>
                         </p>
 
                     </div>
@@ -95,7 +108,7 @@ export default function Home() {
 
                         <div className="flex flex-col my-auto">
                             <h4 className="md:text-lg font-medium">Samantha accepted your booking request</h4>
-                            <p>$350 <span className="h-full w-[1px] bg-black" /> <span className='text-gray-500'>For June 20th, 2023</span></p>
+                            <p className='text-sm md:text-base'>$350 <span className="h-full w-[1px] bg-black" /> <span className='text-gray-500'>For June 20th, 2023</span></p>
 
                         </div>
 
@@ -131,9 +144,9 @@ export default function Home() {
                 <div className="flex flex-col p-4 md:p-8">
                     <div className="flex flex-col ">
                         <h4 className="md:text-lg font-medium">You have cancelled your Natural glam look appointment.</h4>
-                        <p className="flex gap-2">
+                        <p className="flex gap-2 text-sm md:text-base">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-credit-card text-[#CC0087]"><rect width="20" height="14" x="2" y="5" rx="2" /><line x1="2" x2="22" y1="10" y2="10" /></svg>
-                            <span>$70 charged</span>
+                            <span className="font-medium my-auto">$70 charged</span>
                         </p>
 
                     </div>
